b2cc7988187c2c9130bd8ecd3594369f10d49284
AplinXy9plin <evilberry@icloud.com>
2019-08-21 14:26:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require("express")
 let MongoClient = require("mongodb").MongoClient
 let ObjectId = require("mongodb").ObjectID
 let app = express()
-// let port = 1337 || process.env.PORT
+let port = 1337 || process.env.PORT
 let db_name = "heroku_7k2r28vz"
 let url = "mongodb://admin:q2w3e4r5@ds263917.mlab.com:63917/heroku_7k2r28vz"
 let bodyParser = require("body-parser")
@@ -44,9 +44,25 @@ app.post("/add_conv", (req, res) => {
         var dbo = db.db(db_name)
         var json = req.body;
         json.status = 1;
-        dbo.collection("conv").insertOne(json, (err) => {
-            if(err) throw err;
-            res.json({type: "ok"})
+        dbo.collection('images').insertOne({photo: json.photo}, (err, result) => {
+            if (err) throw err;
+            json.photo = result.insertedId;
+            dbo.collection("conv").insertOne(json, (err) => {
+                if(err) throw err;
+                res.json({type: "ok"})
+            })
+        })
+    })
+})
+
+app.get('/img/:id', (req, res) => {
+    MongoClient.connect(url, (err, db) => {
+        if (err) throw err;
+        var dbo = db.db(db_name)
+        dbo.collection('images').findOne({_id: ObjectId(req.params.id)}, (err, doc) => {
+            if (err) throw err;
+            res.contentType(doc.contentType);
+            res.send(doc.data.buffer);
         })
     })
 })
@@ -73,4 +89,4 @@ app.get("/remove", (req, res) => {
     })
 })
 
-app.listen(process.env.PORT)
+app.listen(port)
